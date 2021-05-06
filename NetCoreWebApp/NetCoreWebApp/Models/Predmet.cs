1c3b1bbdbf607374e44ec19fbb3f8f41610ae9c9
Harun Hadzic <harun.hadzic17@gmail.com>
2021-05-06 15:18:15 +0200

    Pogledi LV

--- NetCoreWebApp/NetCoreWebApp/Models/Predmet.cs
+++ NetCoreWebApp/NetCoreWebApp/Models/Predmet.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -14,11 +15,15 @@ namespace NetCoreWebApp.Models
         [Key]
         [Required]
         public int ID { get; set; }
-        
+
         [Required]
+        [StringLength(maximumLength: 50, MinimumLength = 3, ErrorMessage = "Naziv predmeta smije imati između 3 i 50 karaktera!")]
+        [RegularExpression(@"[0-9| |a-z|A-Z]*", ErrorMessage = "Dozvoljeno je samo korištenje velikih i malih slova,brojeva i razmaka!")]
+        [DisplayName("Naziv predmeta:")]
         public string Naziv { get; set; }
         
         [Required]
+        [Range(1,10)]
         public double ECTS { get; set; }
         
         [NotMapped]
